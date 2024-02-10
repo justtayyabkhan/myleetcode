b643251653465e3a262b5c2e93317bdb299bd350
Tayyab Khan <141571123+justtayyabkhan@users.noreply.github.com>
2024-02-11 00:27:41 +0530

    Time: 3 ms (71.95%) | Memory: 12 MB (18.98%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -1,19 +1,31 @@
 class Solution {
 public:
+    bool findele(vector<int> arr,int target)
+    {
+        int low=0;
+        int high=arr.size()-1;
+        bool ans=false;
+        while(low<=high)
+        {
+            int mid=(low+high)/2;
+            if(arr[mid]==target)
+            {
+                return true;
+            }
+            else if(arr[mid]>target) high=mid-1;
+            else low=mid+1;
+        }
+        return false;
+    }
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int m=matrix.size();
         int n=matrix[0].size();
         bool ans=false;
         for(int i=0;i<m;i++)
         {
-            for(int j=0;j<n;j++)
-            {
-                if(matrix[i][j]==target)
-                {
-                    return true;
-                }
-            }
+            ans=findele(matrix[i],target);
+            if(ans==true) return true;
         }
-        return false;
+        return ans;
     }
 };
