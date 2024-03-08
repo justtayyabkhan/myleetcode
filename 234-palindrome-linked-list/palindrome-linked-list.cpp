0fd6c7951b055fe71ce599e066272bcb6a53f7c4
Tayyab Khan <141571123+justtayyabkhan@users.noreply.github.com>
2024-03-09 00:48:57 +0530

    Time: 168 ms (65.68%) | Memory: 125.8 MB (27.83%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/palindrome-linked-list.cpp
@@ -21,21 +21,11 @@ public:
             sz++;
         }
         temp1=head;
-        ListNode* reversedLL=new ListNode(-1);
-        ListNode* temp2=reversedLL;
-        while(!st.empty())
-        {
-            ListNode* temp3=new ListNode(st.top());
-            st.pop();
-            temp2->next=temp3;
-            temp2=temp3;
-        }
-        temp2=reversedLL->next;
         for(int i=0;i<sz;i++)
         {
-            if(temp1->val!=temp2->val) return false;
+            if(temp1->val!=st.top()) return false;
             temp1=temp1->next;
-            temp2=temp2->next;
+            st.pop();
         }
         return true;
     }
